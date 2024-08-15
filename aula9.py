0ceca413f7e3a8d1218a43477de5966ae42e25c0
Fillipi alves <fillipialves3@gmail.com>
2024-08-15 16:46:37 -0300

    Add files via upload

---
+++ aula9.py
@@ -0,0 +1,60 @@
+# # EXERCÍCIO DA AULA 8 HOSPEDAGEM DE HOTEL
+
+
+
+# cliente1_nome = input('Digite o nome do Cliente 1: ')
+# cliente1_idade = int(input('Digite a idade do Cliente 1: '))
+
+# cliente2_nome = input('Digite o nome do Cliente 2: ')
+# cliente2_idade = int(input('Digite a idade do Cliente 2: '))
+
+# cliente3_nome = input('Digite o nome do Cliente 3: ')
+# cliente3_idade = int(input('Digite a idade do Cliente 3: '))
+
+
+# print('Tipos de Quarto: Simples, Duplo, Luxo')
+
+# cliente1_quarto = input('Escolha o quarto para o Cliente 1: ')
+# cliente2_quarto = input('Escolha o quarto para o Cliente 2: ')
+# cliente3_quarto = input('Escolha o quarto para o Cliente 3: ')
+
+# # preço quartos
+# preco_simples = 100
+# preco_duplo = 150
+# preco_luxo = 250
+
+# cliente1_dias = int(input(f'Quantos dias o Cliente 1 ({cliente1_nome}) ficará hospedado? '))
+# cliente2_dias = int(input(f'Quantos dias o Cliente 2 ({cliente2_nome}) ficará hospedado? '))
+# cliente3_dias = int(input(f'Quantos dias o Cliente 3 ({cliente3_nome}) ficará hospedado? '))
+
+
+# if cliente1_quarto == 'simples':
+#     valor_cliente1 = preco_simples * cliente1_dias
+# elif cliente1_quarto == 'duplo':
+#     valor_cliente1 = preco_duplo * cliente1_dias
+# else:
+#     valor_cliente1 = preco_luxo * cliente1_dias
+
+# if cliente2_quarto == 'simples':
+#     valor_cliente2 = preco_simples * cliente2_dias
+# elif cliente2_quarto == 'duplo':
+#     valor_cliente2 = preco_duplo * cliente2_dias
+# else:
+#     valor_cliente2 = preco_luxo * cliente2_dias
+
+# if cliente3_quarto == 'simples':
+#     valor_cliente3 = preco_simples * cliente3_dias
+# elif cliente3_quarto == 'duplo':
+#     valor_cliente3 = preco_duplo * cliente3_dias
+# else:
+#     valor_cliente3 = preco_luxo * cliente3_dias
+
+
+# print(f'Cliente 1: {cliente1_nome}, {cliente1_idade} anos, Quarto {cliente1_quarto}, {cliente1_dias} dias')
+# print(f'Valor Total Cliente 1: R$ {valor_cliente1:.2f}')
+
+# print(f'Cliente 2: {cliente2_nome}, {cliente2_idade} anos, Quarto {cliente2_quarto}, {cliente2_dias} dias')
+# print(f'Valor Total Cliente 2: R$ {valor_cliente2:.2f}')
+
+# print(f'Cliente 3: {cliente3_nome}, {cliente3_idade} anos, Quarto {cliente3_quarto}, {cliente3_dias} dias')
+# print(f'Valor Total Cliente 3: R$ {valor_cliente3:.2f}')
